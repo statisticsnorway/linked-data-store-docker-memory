db69aa951575378f62c51cb760913ecd739dd4bc
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2018-09-27 12:22:41 +0200

    docker-compose and run-script improvements.

--- run-dev.sh
+++ run-dev.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-ENV_FILE='docker-compose.env'
-if [ -f $ENV_FILE ]; then
-    export $(grep -v '^#' $ENV_FILE | xargs -0)
+mkdir -p conf
+mkdir -p schemas
+
+if [ "$1" == "clean" ]; then
+  echo "Cleaning existing associated volumes and data"
+  docker-compose down
+  docker volume rm $(docker volume ls -q -f "name=ldsmemory")
+else
+  echo "Reusing existing volumes and data"
 fi
 
-docker-compose -f docker-compose.yml up --remove-orphans
+ENV_FILE='docker-compose.env'
+if [ -f $ENV_FILE ]; then
+    export $(grep -v '^#' $ENV_FILE | envsubst | xargs -0)
+fi
+
+docker-compose up --remove-orphans
